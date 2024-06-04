4e7221c864d6e70329ad6cb8d2c6a283fc7bd8da
Vansh Tyagi <vansht.ug23.ec@nitp.ac.in>
2024-06-04 13:30:39 +0530

    models created

---
+++ src/models/Video.model.js
@@ -0,0 +1,44 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema=new Schema(
+  {
+    videoFile:{
+      type:String, //cloudinaryUrl
+      required:true
+    },
+    thumbnail:{
+      type:String, //cloudinaryUrl
+      required:true
+    },
+    title:{
+      type:String, 
+      required:true
+    },
+    description:{
+      type:String, 
+      required:true
+    },
+    duration:{
+      type:Number, //cloudinaryUrl
+      required:true
+    },
+    views:{
+      type:Number,
+      default:0
+    },
+    isPublished:{
+      type:Boolean,
+      default:true
+    },
+    owner:{
+      type:Schema.Types.ObjectId,
+      ref:"User"
+    }
+  }
+  
+  
+  
+  ,{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video=mongoose.model("Video",videoSchema)
